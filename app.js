f06c49d2786d4089e8349ad462ebda77c0dd0187
Alexander <info@alexandersstudio.uk>
2022-06-25 10:35:08 +0100

    new

---
+++ app.js
@@ -0,0 +1,86 @@
+//Definig variables
+
+const form = document.getElementById('form');
+const text = document.getElementById('input');
+const addBtn = document.getElementById('addBtn');
+const results = document.querySelector('.results__items');
+let task = [];
+
+// Dates
+
+const fullDate = new Date();
+const day = fullDate.getDay();
+const month = fullDate.getMonth();
+const year = fullDate.getFullYear();
+
+startApp();
+
+function startApp() {
+  form.addEventListener('submit', addTask);
+}
+
+// **Function add task
+
+function addTask() {
+    const taskDate = `${day}/${month}/${year}`;
+    
+    if (text.value !== '') {
+
+    const taskObject = {
+      id: Date.now(),
+      text: text.value,
+      date: taskDate,
+      
+    };
+
+    task = [...task, taskObject];
+    console.log(task);
+
+    addNote(taskObject);
+    form.reset();
+  } else {
+    console.log('error');
+  }
+}
+
+// **Function add note inside od the container
+
+function addNote(task) {
+  const note = document.createElement('div');
+  note.classList.add(
+    'note',
+    'grid',
+    'gap-4',
+    'h-[315px]',
+    'w-[342px]',
+    'bg-black-50',
+    'borderBox',
+    'rounded',
+    'p-5'
+  );
+
+  note.innerHTML = `
+    
+    <div class="date">
+    <img src="/img/calendar.svg" alt="" />
+    <p>${task.date}</p>
+  </div>
+  <div class="content">
+    <p>
+      ${task.text}
+    </p>
+  </div>
+
+  <div class="bottom">
+    <div class="tag" id="tag">
+      <p>new</p>
+    </div>
+
+    <button class="remove" id="remove">
+      <img src="/img/trash.svg" alt="" />
+    </button>
+  </div>
+    `;
+
+  results.appendChild(note);
+}
